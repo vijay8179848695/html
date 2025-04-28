c4b9a347f4c243194b95bb39b09e5ad6e69a75fc
vijay8179848695 <bhaskarvijay561@gmail.com>
2025-04-29 00:15:11 +0530

    update

---
+++ JAVASCRIPT/operators/script.js
@@ -0,0 +1,64 @@
+//Arithmetic operators
+let a = 434,b = 43;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//logical operators
+console.log((a>43) && (b<54));
+console.log((a>43)||(b<34));
+console.log(!(a<500));
+
+//Relational operators
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+a = "a", b = 2;
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
+
+//Assignment operators
+a = 5, b = 3;
+console.log(a += 1);
+console.log(a -= 1);
+console.log(a *= 1);
+console.log(a /= 1);
+console.log(a %= 3);
+console.log(a **= 2);
+
+
+// unary operators
+// unary +  -- it will convert any value into number
+console.log(2 + '2')//22
+console.log(2 + +'2')//4
+// increment ++  -- will increase value by 1
+a = 2;
+a++;
+console.log(a)
+//++a  -- pre increment -- update, assign
+//a++ -- post increment -- assign, update
+a = 5;
+//let x = ++a;
+//let x = a++;
+let x = ++a + a++ + ++a;
+console.log(x)
+//--a  -- pre increment -- update, assign
+//a-- -- post increment -- assign, update
+a = 5, b = 2;
+// x = a++ + --b + ++a + b--; //5+1+7+1
+
+x = a++ + b;
+console.log(x, a)
+
+a = 5, b = 2;
+// ternary operator
+const c = (a >= b) ? a : b;
+console.log(c)
+
+let res=(a % 2 == 0) ? "even":"odd";
+console.log(res)
---
+++ JAVASCRIPT/strings/script.js
@@ -0,0 +1,39 @@
+console.log("I am Vijay");
+console.log("Naresh IT")
+
+
+let a=4;
+b=a*a;
+console.log("the result is "+b)
+console.log(typeof b)
+
+
+
+//math functions
+console.log(Math.abs(-345));
+console.log(Math.sqrt(81));
+console.log(Math.cbrt(729));
+console.log(Math.ceil(5.4));
+console.log(Math.floor(5.4));
+console.log(Math.round(5.4));
+console.log(Math.random());
+console.log(Math.max(7,6,8,5,9,4));
+console.log(Math.min(7,6,5,3,1));
+
+
+
+//strings
+let s=("Vijay");
+console.log(s.length);
+console.log(s. toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.charAt(4));
+console.log(s.indexOf('V'));
+console.log(s.concat("-developer"));
+console.log(s.includes('Vi'));
+let str1="js is easy";
+console.log(str1.split(" "));
+console.log(s.startsWith('V'));
+console.log(s.endsWith('y'));
+console.log(s.replace('a','0'));
+console.log(s.slice(3));
